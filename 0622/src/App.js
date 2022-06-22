7a7c7da50f51dc8ce257da304fd76db860b7a9d0
WinterKwon <winterkwon7017@gmail.com>
2022-06-22 11:52:39 +0900

    react-redux 사용하기

--- 0622/src/App.js
+++ 0622/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import {useReducer} from 'react';
-
+import {useSelector} from 'react-redux';
 function Left1(props) {
   return (
     <div>
       <h1>Left1</h1>
-      <Left2 onUp={() => {
-          props.onUp();
-        }}
-></Left2>
+      <Left2></Left2>
     </div>
   );
 }
@@ -16,11 +12,7 @@ function Left2(props) {
   return (
     <div>
       <h1>Left2</h1>
-      <Left3
-        onUp={() => {
-          props.onUp();
-        }}
-      ></Left3>
+      <Left3></Left3>
     </div>
   );
 }
@@ -30,7 +22,7 @@ function Left3(props) {
       <h1>Left3</h1>
       <button
         onClick={() => {
-          props.onUp();
+          
         }}
       >
         +
@@ -38,12 +30,11 @@ function Left3(props) {
     </div>
   );
 }
-
 function Right1(props) {
   return (
     <div>
       <h1>Right1</h1>
-      <Right2  count={props.count}></Right2>
+      <Right2></Right2>
     </div>
   );
 }
@@ -51,51 +42,30 @@ function Right2(props) {
   return (
     <div>
       <h1>Right2</h1>
-      <Right3  count={props.count}></Right3>
+      <Right3></Right3>
     </div>
   );
 }
 function Right3(props) {
+  // useSelector(state =>{console.log(state)})  //state안에 value 있는 것 확인하고
+  const value =  useSelector(state =>{
+    return state.value;
+  })
   return (
     <div>
       <h1>Right3</h1>
-      {props.count}
+      {value}
     </div>
   );
 }
-// //App()외부로 분리시킴
-// function countReducer (state, action) {
-//   if (action.type === "UP") {
-//     return { ...state, value: state.value + action.step };
-//   }
-//   return state;
-// };
-
 export default function App() {
-  const countReducer = (state, action) => {
-    if (action.type === "UP") {
-      return { ...state, value: state.value + action.step };
-    }
-    return state;
-  };
-  const initialState = {
-    value: 0
-  };
-  const [count, dispatch] = useReducer(countReducer, initialState);
-  const up = (step) => {
-    return { type: "UP", step: step };
-  };  //action creator (redux의 용어. useReducer에서 쓰지는 않음)
 
   return (
     <div id="app">
       <h1>Root</h1>
       <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-        <Left1 onUp={
-          ()=>{
-            dispatch(up(1));  //dispatch안에 {type: 'up', step : 1} 이런 로직을 위의 up 로직->action으로 분리
-          }
-        }></Left1>
-        <Right1 count={count.value}></Right1>  
+        <Left1></Left1>
+        <Right1></Right1>
       </div>
     </div>
   );
